0d8af41f98c2fa4e07836750b3b478e929b7b067
Troy Butler <troy.butler@dynology.com>
2019-09-30 19:01:19 -0400

    Completed Project Setup

--- VOD.Database/Migrations/20190930225257_Initial.cs
+++ VOD.Database/Migrations/20190930225257_Initial.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace VOD.Database.Migrations
 {
-    public partial class Initial : Migration
+    public partial class Initial : Migration 
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
