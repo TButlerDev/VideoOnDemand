d458c15276a35b559a192a7442dcdf13a1598df5
Troy Butler <troy.butler@dynology.com>
2019-10-02 19:35:35 -0400

    update-DTO ViewModels added

---
+++ VOD.Common/DTOModels/UI/InstructorDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VOD.Common.DTOModels.UI
+{
+    public class InstructorDTO
+    {
+        public string InstructorName { get; set; }
+        public string InstructorDescription { get; set; }
+        public string InstructorAvatar { get; set; }
+
+    }
+}
---
+++ VOD.UI/Models/MembershipViewModel/DashboardViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VOD.Common.DTOModels.UI;
+
+namespace VOD.UI.Models.MembershipViewModel
+{
+    public class DashboardViewModel
+    {
+        public List<List<CourseDTO>> Courses { get; set; }
+    }
+}
